228c41f16941680510a23f5aecd691e102a865ef
cyanchu <cyanchu@gmail.com>
2015-04-24 21:19:20 +0800

    Update ip2do.cpp

--- ip2do.cpp
+++ ip2do.cpp
@@ -6,55 +6,51 @@
 #include<algorithm>
 using namespace std;
 #define SIZE 1024
-char line[SIZE];
-char *sourceFile;
-char *dirFile;
-char *disFile;
-//map<string,string> *IP2DO;
+
 map<string,string> iptodo;
 
 void map_insert(map<string,string> *ID2DO,string ip,string domain){
 	ID2DO->insert(map<string,string>::value_type(ip,domain));
 }
-
-void readDir(){
+void readDir(char* dirFile ){
+	char line[SIZE] = {0};
+	string strDomain = "";
+	string strIP = "";
 	fstream fin;
 	fin.open(dirFile,ios::in);
 	while(fin.getline(line,sizeof(line),'\n')){
- 		char buffer[1024];
-		strcpy(buffer,line);
-		char *domain = strstr(buffer,":");
-		int point = domain - buffer;
-		string ip = buffer;
-		ip = ip.substr(0,point);
-		*domain++;
-		map_insert(&iptodo,ip,domain);	
+ 		
+		strDomain = strtok(line, ":");
+		if(strDomain != ""){
+			strIP = strtok(NULL, ":");
+		}
+		map_insert(&iptodo,strIP,strDomain);	
 		
 	}
+	fin.close();
 }
 int main(int argc, char **argv){
+	char line[SIZE] = {0};
+	string buffer;
 	//read argv
-	if(argc == 1 || argc == 2 || argc == 3){
+	if((argc == 1) || (argc == 2) || (argc == 3)){
 		printf("Please tell me how to do it...\nEx: ./ip2do [domain list] [source config] [new config]\n");
 		return 1;
 	}
-	dirFile = argv[1];
-	sourceFile = argv[2];
-	disFile = argv[3];
-	readDir();
+	readDir(argv[1]);
 	//start main
 	fstream fin;
 	fstream fp;
-	fin.open(sourceFile,ios::in);
-	fp.open(disFile,ios::out);
+	fin.open(argv[2],ios::in);
+	fp.open(argv[3],ios::out);
+	int found = -1;
 	while(fin.getline(line,sizeof(line),'\n')){
-		char a[1024];
-		string buffer = strcpy(a,line);	
+
+		buffer = line;
 		map<string,string>::iterator iter;	
 		for(iter= iptodo.begin();iter!=iptodo.end();iter++){
 			string ip = iter->first;
 			string domain = iter->second;
-			int found;
 			found = buffer.find(ip);
 			if(found != -1){
 				buffer.replace(found,ip.size(),domain);
@@ -63,6 +59,7 @@ int main(int argc, char **argv){
 		fp<<buffer<<endl;	
 	}
 	fp.close();
+	fin.close();
 	return 0;	
 }
 
